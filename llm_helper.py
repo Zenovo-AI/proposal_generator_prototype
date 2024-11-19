50cb621bb1f6204c0edea1da6b0a50ebb402cdc0
Okafor Chibuzor <134807240+CertifiedAuthur@users.noreply.github.com>
2024-11-19 09:16:22 +0100

    cleaning up the repository

--- llm_helper.py
+++ llm_helper.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from langchain_groq import ChatGroq
+from llm_helper import ChatGroq
 
+# Initialize and retrieve api_key
+st.session_state.api_key = st.session_state.api_key or st.sidebar.text_input("Enter your Groq API KEY")
+if not st.session_state.api_key:
+    st.error("Please enter a valid API key")
+    st.stop()
 
-llm = ChatGroq(groq_api_key=st.session_state.api_key, model_name = "Llama-3.1-70b-versatile")
+llm = ChatGroq(groq_api_key=st.session_state.api_key, model_name="Llama-3.1-70b-versatile")
